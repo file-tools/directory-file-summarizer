b45a3d4fa3a3216eceeab285984f53a79c06026c
Reess Kennedy <reesskennedy2@gmail.com>
2023-08-06 12:50:29 -0400

    Adding more config options to control output+ emojis ...

--- config-sample.py
+++ config-sample.py
@@ -5,4 +5,12 @@ folder_path = ""
 output_file = ""
 
 # Indentation
-indentation = "↳   "
+indentation = "↳  "
+
+# Separators between levels (set to None by default)
+space_d0 = "\n"  # Add a space between the directories in the root path
+space_d1 = None
+space_d2 = None
+space_d3 = None
+# You can continue adding space_d4, space_d5, etc. as needed
+
--- scan-hierarchy.py
+++ scan-hierarchy.py
@@ -1,14 +1,20 @@
 import os
-from config import folder_path, output_file, indentation
+
+# Try to import configuration from config.py
+try:
+    from config import folder_path, output_file, indentation, space_d0, space_d1, space_d2, space_d3
+except ImportError:
+    print("Configuration not found. Make sure you have a valid config.py.")
+    exit(1)
 
 def count_files(folder_path):
     folder_info = {}  # Dictionary to store folder names and file counts
 
     for root, dirs, files in os.walk(folder_path):
         if root == folder_path:
-            level = 0
+            level = 1  # Start counting from 1 for the first directory level
         else:
-            level = root[len(folder_path) + len(os.path.sep):].count(os.path.sep)
+            level = root[len(folder_path) + len(os.path.sep):].count(os.path.sep) + 1
 
         if level <= 6:
             folder_name = os.path.basename(root)
@@ -23,13 +29,23 @@ def count_files(folder_path):
 
 def write_to_file(folder_info, output_file):
     with open(output_file, 'w', encoding='utf-8') as f:
+        prev_level = -1  # To track the previous level
+
         for folder, (file_count, dir_count, level) in sorted(folder_info.items()):
-            indentation_string = indentation * level
-            
+            indentation_string = indentation * (level - 1)  # Adjust indentation based on adjusted levels
+
+            # Add separators based on the level difference and if they are defined
+            separators = [space_d0, space_d1, space_d2, space_d3]  # Add more separator variables as needed
+            separator = separators[level - 1] if level <= len(separators) and separators[level - 1] is not None else ""
+            if separator:
+                f.write(f"{separator}\n")
+
+            folder_line = f"{indentation_string}{os.path.basename(folder)} "
+
             if dir_count > 0:
-                folder_line = f"{indentation_string}{os.path.basename(folder)} ({dir_count} folders, {file_count} files)"
-            else:
-                folder_line = f"{indentation_string}{os.path.basename(folder)} ({file_count} files)"
+                folder_line += f"{dir_count} 📁"
+            if file_count > 0:
+                folder_line += f" {file_count} 🖼️"
             
             f.write(f"{folder_line}\n")
 
